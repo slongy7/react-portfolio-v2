a57672c87520eb7694febd0f4d03819e91dbd270
slongy7 <79759725+slongy7@users.noreply.github.com>
2021-10-11 18:04:58 -0400

    Updates

---
+++ precache-manifest.6554206ab2023238162bf2bb0e371933.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9a1cae6dd45a01f8211df40e100eea",
+    "url": "/react-portfolio-v2/index.html"
+  },
+  {
+    "revision": "4c01849f9e54d7190082",
+    "url": "/react-portfolio-v2/static/css/main.a3592a7f.chunk.css"
+  },
+  {
+    "revision": "ccdf87bc2252e80d8048",
+    "url": "/react-portfolio-v2/static/js/2.6cf05abb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio-v2/static/js/2.6cf05abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c01849f9e54d7190082",
+    "url": "/react-portfolio-v2/static/js/main.0a893d73.chunk.js"
+  },
+  {
+    "revision": "3df2e4d06bb6f44a0ea1",
+    "url": "/react-portfolio-v2/static/js/runtime-main.e26d9b75.js"
+  },
+  {
+    "revision": "3921d6a992b9dec4b4b6a56f854b4d19",
+    "url": "/react-portfolio-v2/static/media/Headshot.3921d6a9.jpeg"
+  },
+  {
+    "revision": "a39808f9a4e5fd33eb09b200de53ac77",
+    "url": "/react-portfolio-v2/static/media/resume_justin_long.a39808f9.pdf"
+  }
+]);
